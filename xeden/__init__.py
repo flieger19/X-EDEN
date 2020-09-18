943d6a2c8b528b66cafed4b9f9b98750cd3b7064
Constantin Dullo <constantin.dullo@me.com>
2020-09-18 19:51:39 +0200

    Adding description to the package init file

--- xeden/__init__.py
+++ xeden/__init__.py
@@ -8,3 +8,8 @@ VERSION = (0, 0, 1)
 __version__ = ".".join([str(string) for string in VERSION])
 
 __title__ = "X-EDEN"
+
+__description__ = (
+    "Embedded Development for Xcode"
+    "CMSIS, HAL, FreeRTOS, STM32Cube, ARM, STMicroelectronics (STM32)"
+)
