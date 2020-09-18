9de58eb8e0abb2cca16aaee6116fc6d1d8624458
Constantin Dullo <constantin.dullo@me.com>
2020-09-18 20:33:11 +0200

    Adding getopt to the main function

--- xeden/__main__.py
+++ xeden/__main__.py
@@ -1,11 +1,25 @@
 """
 Documentation
 """
+import getopt
 import sys
 
 
 def main(argv=sys.argv):
     exit_code = 0
+
+    try:
+        opts, args = getopt.getopt(argv, "h", ["help"])
+    except getopt.GetoptError as error:
+        # print help information and exit:
+        print(error)  # will print something like "option -a not recognized"
+        usage()
+        exit_code = 2
+        return exit_code
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            return exit_code
 
     return exit_code
 
