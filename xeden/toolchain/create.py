47031297f1b0edeb47f7e99e6bf26be512ea33b1
Constantin Dullo <constantin.dullo@me.com>
2020-09-19 17:34:55 +0200

    Adding function directory maker to the create source file

--- xeden/toolchain/create.py
+++ xeden/toolchain/create.py
@@ -9,3 +9,14 @@ import shutil
 DEFAULT_TOOL = "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/"
 XEDEN_TOOL = os.path.expanduser('~') + "/Library/Developer/Toolchains/X-EDEN.xctoolchain/"
 CURRENT_DIR = os.path.abspath(os.getcwd())
+
+
+def directory_maker(path):
+    """
+    Makes specified directory
+    :param path: directory to make
+    """
+    try:
+        os.makedirs(path)
+    except OSError:
+        print("Creation of the directory %s failed" % path)
