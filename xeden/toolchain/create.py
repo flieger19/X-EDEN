751ba973aae7c6c09e2afdf9300f94bd20d9657f
Constantin Dullo <constantin.dullo@me.com>
2020-09-19 18:27:20 +0200

    Implementing directory iterator function of the create source file

--- xeden/toolchain/create.py
+++ xeden/toolchain/create.py
@@ -28,3 +28,24 @@ def directory_iterator(source, target):
     :param source: Directory to iterate through
     :param target: Directory to symlink files to
     """
+    for file in os.listdir(source):
+        filename = os.fsdecode(file)
+        path_source = source + bytes("/", 'utf-8') + file
+        path_target = target + bytes("/", 'utf-8') + file
+        if os.path.isdir(path_source):
+            directory_maker(os.fsdecode(path_target))
+            directory_iterator(os.fsencode(path_source), os.fsencode(path_target))
+        elif os.path.isfile(path_source):
+            try:
+                os.symlink(path_source, path_target)
+            except:
+                print("Symlink Error")
+        elif os.access(path_source, os.X_OK):
+            try:
+                os.symlink(path_source, path_target)
+            except:
+                print("Symlink Error")
+        elif os.path.islink(path_source):
+            continue
+        else:
+            print("Special file ", path_source)
