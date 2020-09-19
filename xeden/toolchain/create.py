93d7525dc3aaa8f0e296ec0d753cad52eaaaaaf0
Constantin Dullo <constantin.dullo@me.com>
2020-09-19 17:31:43 +0200

    Adding constants to the create source file

--- xeden/toolchain/create.py
+++ xeden/toolchain/create.py
@@ -5,3 +5,7 @@ Documentation
 import sys
 import os
 import shutil
+
+DEFAULT_TOOL = "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/"
+XEDEN_TOOL = os.path.expanduser('~') + "/Library/Developer/Toolchains/X-EDEN.xctoolchain/"
+CURRENT_DIR = os.path.abspath(os.getcwd())
