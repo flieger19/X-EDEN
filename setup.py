b85d2887b3cc92ea885c3acf728bac74df032fd1
Constantin Dullo <constantin.dullo@me.com>
2020-09-29 15:22:54 +0200

    Adding main function to the setup source

--- setup.py
+++ setup.py
@@ -3,3 +3,14 @@ Documentation, License etc.
 
 @package X-EDEN Xcode Embedded DEvelopmeNt
 """
+import sys
+
+
+def main(argv=sys.argv):
+    exit_code = 0
+
+    return exit_code
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
